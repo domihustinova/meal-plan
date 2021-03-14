0b1b5e9282262b729adcbe7e4d3c10e75e92f0e8
domi <domi.hustinova@gmail.com>
2021-03-14 20:04:12 +0100

    refactor: add more functionality to helper function

--- src/components/Jumbotron/services/helpers.js
+++ src/components/Jumbotron/services/helpers.js
@@ -1,4 +1,64 @@
-export const calculateTdee = ({
+const getBmr = (gender, age, weight, height) => {
+  const weightFactor = 10;
+  const heightFactor = 6.25;
+  const ageFactor = 5;
+
+  // Mifflin St Jeor Formula
+  let bmr = weightFactor * weight + heightFactor * height - ageFactor * age;
+
+  return gender === "female" ? (bmr -= 161) : (bmr += 5);
+};
+
+const getTdee = (activity, bmr) => {
+  switch (activity) {
+    case "sedentary":
+      return Math.round(bmr * 1.2);
+    case "light":
+      return Math.round(bmr * 1.375);
+    case "moderate":
+      return Math.round(bmr * 1.55);
+    case "heavy":
+      return Math.round(bmr * 1.725);
+    case "extreme":
+      return Math.round(bmr * 1.9);
+    default:
+      return null;
+  }
+};
+
+const macroRatios = [
+  {
+    name: "lowCarb",
+    protein: 40,
+    carb: 20,
+    fat: 40,
+  },
+  {
+    name: "moderateCarb",
+    protein: 30,
+    carb: 35,
+    fat: 35,
+  },
+  {
+    name: "highCarb",
+    protein: 30,
+    carb: 50,
+    fat: 20,
+  },
+];
+
+const getMacros = (tdee) => {
+  const macros = macroRatios.map((ratio) => ({
+    name: ratio.name,
+    protein: Math.floor((tdee * ratio.protein) / 100 / 4),
+    carb: Math.floor((tdee * ratio.carb) / 100 / 4),
+    fat: Math.floor((tdee * ratio.fat) / 100 / 9),
+  }));
+
+  return macros;
+};
+
+export const getCalories = ({
   gender,
   age,
   weight,
@@ -6,33 +66,8 @@ export const calculateTdee = ({
   activity,
   bodyfat,
 }) => {
-  let weightFactor = 10;
-  let heightFactor = 6.25;
-  let ageFactor = 5;
-  let tdee;
+  const bmr = getBmr(gender, age, weight, height);
+  const tdee = getTdee(activity, bmr);
 
-  // Mifflin St Jeor Formula
-  let bmr = weightFactor * weight + heightFactor * height - ageFactor * age;
-
-  gender === "female" ? (bmr -= 161) : (bmr += 5);
-
-  switch (activity) {
-    case "sedentary":
-      tdee = Math.round(bmr * 1.2);
-      break;
-    case "light":
-      tdee = Math.round(bmr * 1.375);
-      break;
-    case "moderate":
-      tdee = Math.round(bmr * 1.55);
-      break;
-    case "heavy":
-      tdee = Math.round(bmr * 1.725);
-      break;
-    case "extreme":
-      tdee = Math.round(bmr * 1.9);
-      break;
-    default:
-  }
-  return tdee;
+  return getMacros(tdee);
 };
