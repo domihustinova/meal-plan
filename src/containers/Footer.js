cc64f8ab609967c1f164805262e8fb6a04ed4ed6
domi <domi.hustinova@gmail.com>
2021-03-28 22:41:05 +0200

    fix: illustrations attribution

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -7,7 +7,7 @@ export function FooterContainer() {
     <Footer>
       <Footer.Text>
         Coded with &hearts; by{" "}
-        <a href="https://github.com/domihustinova">Domi</a>. Designed by{" "}
+        <a href="https://github.com/domihustinova">Domi</a>. Illustrations by{" "}
         <a href="http://www.freepik.com">pch.vector / Freepik</a>
       </Footer.Text>
     </Footer>
