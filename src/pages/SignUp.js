262ee7c3989813d8fd2c795bf9785a8b3f54f2a6
domi <domi.hustinova@gmail.com>
2021-03-30 21:23:15 +0200

    feat: add Header and Footer to pages

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,24 @@
 import React from "react";
+import styled from "styled-components/macro";
+
+import { HeaderContainer } from "../containers/Header";
+import { FooterContainer } from "../containers/Footer";
+
+export const Main = styled.main`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+`;
 
 export default function SignUp() {
-  return <p>Hello from the signup!</p>;
+  return (
+    <>
+      <HeaderContainer />
+      <Main />
+      <FooterContainer />
+    </>
+  );
 }
