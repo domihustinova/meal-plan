cd4d645f7190e2adb24b88752821e2b46e6dd0e3
domi <domi.hustinova@gmail.com>
2021-05-08 21:58:15 +0200

    feat: change section from planner to editor

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,7 +1,7 @@
+export const EDITOR = "/editor";
 export const HOME = "/";
 export const MEALS = "/meals";
 export const OVERVIEW = "/overview";
-export const PLANNER = "/planner";
 export const PROFILE = "/profile";
 export const RECIPES = "/recipes";
 export const SIGN_IN = "/signin";
