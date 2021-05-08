5fbc805fdc04d61e2f861194bff3862827d80a38
domi <domi.hustinova@gmail.com>
2021-05-08 21:57:14 +0200

    feat: change sidebar style

--- src/components/Sidebar/styles/Sidebar.js
+++ src/components/Sidebar/styles/Sidebar.js
@@ -1,8 +1,8 @@
 import styled from "styled-components/macro";
-import { Link as ReactRouterLink } from "react-router-dom";
 
 export const Container = styled.div`
   background: ${({ theme }) => theme.background.sidebar};
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
 
   a {
     display: block;
@@ -10,56 +10,47 @@ export const Container = styled.div`
   }
 
   & > div {
-    width: 300px;
+    width: 125px;
     height: 100vh;
   }
 `;
 
-export const Frame = styled.div`
-  padding: 24px;
+export const Group = styled.div`
+  padding: 15px;
+  margin-top: 20px;
+`;
+
+export const ItemGroup = styled.div`
   display: flex;
   flex-direction: column;
-`;
-
-export const Group = styled.div``;
-
-export const Title = styled.h3`
-  padding: 48px 48px;
-  padding-bottom: 0;
-  text-align: center;
-`;
-
-export const Item = styled(ReactRouterLink)`
+  align-items: center;
+  padding: 15px 10px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  font-weight: 500;
+  background: ${(props) =>
+    props.selected
+      ? props.theme.background.sidebarItemSelected
+      : props.theme.background.sidebarItem};
   color: ${(props) =>
     props.selected
       ? props.theme.colorText.link.sidebarSelected
       : props.theme.colorText.link.sidebar};
-  padding: 14px 24px;
-  border-radius: 30px;
-  margin-bottom: 10px;
-  background: ${(props) => (props.selected ? "#fff" : "")};
-  font-weight: 400;
-  text-decoration: none;
   transition: all ${({ theme }) => theme.durationNormal} ease-in-out;
 
   &:hover {
-    cursor: pointer;
     color: ${({ theme }) => theme.colorText.link.sidebarHover};
+    cursor: pointer;
   }
+`;
+
+export const Item = styled.span`
+  text-align: center;
+  font-size: 12px;
+  text-decoration: none;
 `;
 
 export const ItemIcon = styled.span`
-  margin-right: 24px;
-  color: ${({ theme }) => theme.sidebar.icon};
-`;
-
-export const LogoutLink = styled.a`
-  color: ${({ theme }) => theme.colorText.link.sidebar};
-  padding: 14px 24px;
-  transition: all ${({ theme }) => theme.durationNormal} ease-in-out;
-
-  &:hover {
-    cursor: pointer;
-    color: ${({ theme }) => theme.colorText.link.sidebarHover};
-  }
+  margin-bottom: 3px;
+  font-weight: 700;
 `;
--- src/constants/sidebarItems.js
+++ src/constants/sidebarItems.js
@@ -1,29 +1,34 @@
 import * as ROUTES from "./routes";
+import bookIcon from "@iconify-icons/akar-icons/book";
+import fileIcon from "@iconify-icons/akar-icons/file";
+import houseLine from "@iconify-icons/ph/house-line";
+import noodleBowl from "@iconify-icons/carbon/noodle-bowl";
+import personIcon from "@iconify-icons/bi/person";
 
 export const SIDEBAR_ITEMS = [
   {
     name: "Overview",
-    icon: "home",
+    icon: houseLine,
     route: ROUTES.OVERVIEW,
   },
   {
-    name: "Meal Planner",
-    icon: "file",
-    route: ROUTES.PLANNER,
+    name: "Meal Plan Editor",
+    icon: fileIcon,
+    route: ROUTES.EDITOR,
   },
   {
     name: "Profile",
-    icon: "weight",
+    icon: personIcon,
     route: ROUTES.PROFILE,
   },
   {
     name: "Meals",
-    icon: "leaf",
+    icon: noodleBowl,
     route: ROUTES.MEALS,
   },
   {
     name: "Recipes",
-    icon: "book",
+    icon: bookIcon,
     route: ROUTES.RECIPES,
   },
 ];
