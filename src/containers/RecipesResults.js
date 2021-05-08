6635a8cef08b527a19c7d9a51ff05ba5c42413e7
domi <domi.hustinova@gmail.com>
2021-05-08 19:18:00 +0200

    refactor: use RecipeCardContainer

--- src/containers/RecipesResults.js
+++ src/containers/RecipesResults.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Recipe } from "../components";
+import { RecipeCard } from "../components/";
+import { RecipeCardContainer } from "../containers/RecipeCard";
 
-import { getLabel, getRecipeId } from "../helpers/recipes";
+import { getRecipeId } from "../helpers/recipes";
 
 export function RecipesResultsContainer({
   recipesData,
@@ -9,71 +10,15 @@ export function RecipesResultsContainer({
   handleRemoveButton,
 }) {
   return (
-    <Recipe.Container>
-      {recipesData?.hits?.map(({ recipe }) => {
-        const {
-          PROCNT: protein,
-          CHOCDF: carbs,
-          FAT: fat,
-        } = recipe.totalNutrients;
-
-        const recipeId = getRecipeId(recipe.uri);
-
-        return (
-          <Recipe key={recipeId}>
-            {recipe.totalTime !== 0 && (
-              <Recipe.Time>{recipe.totalTime} mins</Recipe.Time>
-            )}
-            <Recipe.Image src={recipe.image} />
-            <Recipe.TextContainer>
-              <Recipe.Title>{getLabel(recipe.label)}</Recipe.Title>
-              <Recipe.EnergyContainer>
-                <Recipe.Energy>
-                  <Recipe.EnergyTitle>Calories</Recipe.EnergyTitle>
-                  <Recipe.EnergyValue>
-                    {Math.floor(recipe.calories / recipe.yield)}
-                  </Recipe.EnergyValue>
-                </Recipe.Energy>
-
-                <Recipe.Energy>
-                  <Recipe.EnergyTitle>{protein.label}</Recipe.EnergyTitle>
-                  <Recipe.EnergyValue>
-                    {Math.floor(protein.quantity / recipe.yield)}
-                    {protein.unit}
-                  </Recipe.EnergyValue>
-                </Recipe.Energy>
-
-                <Recipe.Energy>
-                  <Recipe.EnergyTitle>{carbs.label}</Recipe.EnergyTitle>
-                  <Recipe.EnergyValue>
-                    {Math.floor(carbs.quantity / recipe.yield)}
-                    {carbs.unit}
-                  </Recipe.EnergyValue>
-                </Recipe.Energy>
-
-                <Recipe.Energy>
-                  <Recipe.EnergyTitle>{fat.label}</Recipe.EnergyTitle>
-                  <Recipe.EnergyValue>
-                    {Math.floor(fat.quantity / recipe.yield)}
-                    {fat.unit}
-                  </Recipe.EnergyValue>
-                </Recipe.Energy>
-              </Recipe.EnergyContainer>
-            </Recipe.TextContainer>
-            <Recipe.Footer>
-              <Recipe.FooterView>View Recipe</Recipe.FooterView>
-              <Recipe.FooterAdd
-                onClick={() => handleAddButton(recipeId, recipe)}
-              >
-                +
-              </Recipe.FooterAdd>
-              <Recipe.FooterRemove onClick={() => handleRemoveButton(recipeId)}>
-                -
-              </Recipe.FooterRemove>
-            </Recipe.Footer>
-          </Recipe>
-        );
-      })}
-    </Recipe.Container>
+    <RecipeCard.Container>
+      {recipesData?.hits?.map(({ recipe }) => (
+        <RecipeCardContainer
+          key={getRecipeId(recipe.uri)}
+          recipe={recipe}
+          handleAddButton={handleAddButton}
+          handleRemoveButton={handleRemoveButton}
+        />
+      ))}
+    </RecipeCard.Container>
   );
 }
