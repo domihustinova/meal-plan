e02f1c974ef9adf9275f3e065d0642de68a2bc2e
domi <domi.hustinova@gmail.com>
2021-05-17 17:35:32 +0200

    refactor: create getOptions helper

--- src/constants/mealForm.js
+++ src/constants/mealForm.js
@@ -1,10 +1,9 @@
+import { getOptions } from "../helpers/data";
+
 export const MEAL_TYPE_DATA = {
   breakfast: "Breakfast",
   "lunch/dinner": "Lunch/Dinner",
   snack: "Snack",
 };
 
-export const mealTypeOtions = Object.keys(MEAL_TYPE_DATA).map((value) => ({
-  label: MEAL_TYPE_DATA[value],
-  value,
-}));
+export const mealTypeOtions = getOptions(MEAL_TYPE_DATA);
