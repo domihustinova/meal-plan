87691db8bfa632fdb3c07ee485a9ee4cc67a20f5
domi <domi.hustinova@gmail.com>
2021-05-21 19:34:13 +0200

    chore: replace api url

--- src/constants/recipeApi.js
+++ src/constants/recipeApi.js
@@ -1,5 +1,3 @@
 export const API = {
-  APP_ID: process.env.REACT_APP_APP_ID,
-  APP_KEY: process.env.REACT_APP_APP_KEY,
-  URL: "https://api.edamam.com/",
+  URL: "http://meal-pal-backend.herokuapp.com/",
 };
